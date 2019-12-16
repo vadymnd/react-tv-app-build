f52810260bd2e92578a5bf91a22aaeb38c33cea7
Vadym <51242804+vadymnd@users.noreply.github.com>
2019-12-16 15:07:27 +0200

    v 1.5.0

---
+++ precache-manifest.5b4a164cdbfb98c0eba58a9e630d2cce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6902e4c1c7ef8898e39308a345700c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8eba787664d34a3631f1",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "abf9f4188a0b23fb692a",
+    "url": "/static/css/main.160ef316.chunk.css"
+  },
+  {
+    "revision": "8eba787664d34a3631f1",
+    "url": "/static/js/2.828ee468.chunk.js"
+  },
+  {
+    "revision": "14cfafeab58f243640d9d112b6ecad16",
+    "url": "/static/js/2.828ee468.chunk.js.LICENSE"
+  },
+  {
+    "revision": "abf9f4188a0b23fb692a",
+    "url": "/static/js/main.643b12cd.chunk.js"
+  },
+  {
+    "revision": "200bccd37ddf303e8d63",
+    "url": "/static/js/runtime-main.071eedef.js"
+  },
+  {
+    "revision": "d0b1dc0ad16984ce05f4c9be6204c045",
+    "url": "/static/media/header_background.d0b1dc0a.png"
+  },
+  {
+    "revision": "3b0dace71f98e3355649345fbbe7faee",
+    "url": "/static/media/tv.3b0dace7.png"
+  }
+]);
